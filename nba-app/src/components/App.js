89c28eed50d30f59f0c8bde65e5a3b892e31e0be
Peixuan Jiang <peix8580@esri.com>
2018-01-29 20:31:17 -0800

    Create shotChart

--- nba-app/src/components/App.js
+++ nba-app/src/components/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import {TopNavBar} from "./TopNavBar";
 import '../styles/App.css';
+import {Main} from "./Main";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <TopNavBar/>
+        <Main/>
       </div>
     );
   }
